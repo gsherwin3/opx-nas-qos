3159f1e987e70d8f76adb769599decb010708ba7
Gordon Sherwin <gsherwin@extremenetworks.com>
2017-01-11 14:33:51 -0500

    temp pythonpath mod

--- scripts/bin/base_qos_init.sh
+++ scripts/bin/base_qos_init.sh
@@ -13,7 +13,7 @@
 # See the Apache Version 2.0 License for specific language governing
 # permissions and limitations under the License.
 
-export PYTHONPATH=$OPX_INSTALL_PATH/usr/lib/opx:$OPX_INSTALL_PATH/usr/lib/x86_64-linux-gnu/opx
+export PYTHONPATH=$OPX_INSTALL_PATH/usr/lib/opx:$OPX_INSTALL_PATH/usr/lib/x86_64-linux-gnu/opx:$OPX_INSTALL_PATH/lib/python2.7/site-packages
 
 if [ -f $OPX_INSTALL_PATH/etc/opx/base_qos_no_init ] ; then
    exit 0
